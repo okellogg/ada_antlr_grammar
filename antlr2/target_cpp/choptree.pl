f01d017c516ee08185d2fa461dcd3d388c8dcdca
Oliver Kellogg <okellogg@users.sourceforge.net>
2019-01-23 08:28:41 +0100

    antlr2/target_cpp/choptree.pl - In case of reading from file support more than one tree line in the file.

--- antlr2/target_cpp/choptree.pl
+++ antlr2/target_cpp/choptree.pl
@@ -9,48 +9,60 @@ sub space {
    return "  " x $ilevel;
 }
 
-@ARGV or die "choptree : provide the ANTLR tree string or the name of a file containing it\n";
-
 my @input;
+
+sub choptree {
+   for (my $i = 0; $i < scalar(@input); ++$i) {
+      my $tok = $input[$i];
+      if ($tok eq '(') {
+         print(space . $tok);
+         while ($i + 1 < scalar(@input)) {
+            $input[$i + 1] =~ /^\w/ or last;
+            $tok = $input[++$i];
+            print " $tok";
+         }
+         print "\n";
+         ++$ilevel;
+      } elsif ($tok eq ')') {
+         --$ilevel;
+         print(space . $tok . "\n");
+      } else {
+         print(space . "$tok");
+         while ($i + 1 < scalar(@input)) {
+            $input[$i + 1] =~ /^\w/ or last;
+            $tok = $input[++$i];
+            print " $tok";
+         }
+         print "\n";
+      }
+   }
+}
+
+@ARGV or die "choptree : provide the ANTLR tree string or the name of a file containing it\n";
 
 if (scalar(@ARGV) == 1) {
    if ( $ARGV[0] =~ / \(.*\) / ) {
       @input = split( / /, $ARGV[0] );
+      choptree;
    } else {
       my $filename = $ARGV[0];
       -e "$filename" or die "choptree : argument is neither a tree string nor a file name\n";
       open(IN, "<$filename") or die "choptree : cannot open file $filename\n";
-      my $line = <IN>;
-      chomp $line;
-      @input = split( / /, $line );
+      while (<IN>) {
+         my $line = <IN>;
+         chomp $line;
+	 if ($line =~ /^ *\(/) {
+            @input = split( / /, $line );
+	    choptree;
+	 } else {
+	    print "$line\n";
+	 }
+      }
+      close IN;
    }
 } else {
    @input = @ARGV;
-}
-
-for (my $i = 0; $i < scalar(@input); ++$i) {
-   my $tok = $input[$i];
-   if ($tok eq '(') {
-      print(space . $tok);
-      while ($i + 1 < scalar(@input)) {
-         $input[$i + 1] =~ /^\w/ or last;
-         $tok = $input[++$i];
-         print " $tok";
-      }
-      print "\n";
-      ++$ilevel;
-   } elsif ($tok eq ')') {
-      --$ilevel;
-      print(space . $tok . "\n");
-   } else {
-      print(space . "$tok");
-      while ($i + 1 < scalar(@input)) {
-         $input[$i + 1] =~ /^\w/ or last;
-         $tok = $input[++$i];
-         print " $tok";
-      }
-      print "\n";
-   }
+   choptree;
 }
 
 
